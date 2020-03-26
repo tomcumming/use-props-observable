28f4a239cd5f6ccde3147399b8ec51f92c0324a8
Tom Cumming <mail@tomcumming.co.uk>
2020-03-26 17:35:15 +0000

    Turn into a hook

--- src/index.ts
+++ src/index.ts
@@ -25,47 +25,46 @@ type State<Props> = {
     insideRender: boolean;
 };
 
-export default function reactiveComponent<Props extends object>(
+export default function usePropsObservable<Props extends object>(
+    props: Props,
     render: (props$: Observable<Props>) => Observable<Rendered>,
     compare: (a: Props, b: Props) => boolean = shallowCompare
-): React.StatelessComponent<Props> {
-    return function ReactiveComponent(props: Props): Rendered {
-        const ref = React.useRef<State<Props>>({
-            subj: null as any, // avoid creating this every render
-            last: null,
-            invalidatedCount: 0,
-            insideRender: false
+): Rendered {
+    const ref = React.useRef<State<Props>>({
+        subj: null as any, // limit constructer calls
+        last: null,
+        invalidatedCount: 0,
+        insideRender: false
+    });
+    if(ref.current.subj === null)
+        ref.current.subj = new BehaviorSubject(props);
+
+    const [_invalidatedCount, setInvalidatedCount] = React.useState(ref.current.invalidatedCount);
+
+    React.useEffect(() => () => ref.current.subs?.unsubscribe(), []);
+
+    ref.current.insideRender = true;
+    console.log('render start');
+
+    if(!ref.current.subs) {
+        ref.current.subs = render(ref.current.subj.pipe(distinctUntilChanged(compare))).subscribe(next => {
+            ref.current.last = next;
+            if(!ref.current.insideRender) {
+                ref.current.invalidatedCount += 1;
+                setInvalidatedCount(ref.current.invalidatedCount);
+                console.log('setting state');
+            } else {
+                console.log('skipping state');
+            }
         });
-        if(ref.current.subj === null)
-            ref.current.subj = new BehaviorSubject<Props>(props);
-
-        const [_invalidatedCount, setInvalidatedCount] = React.useState(ref.current.invalidatedCount);
-
-        React.useEffect(() => () => ref.current.subs?.unsubscribe(), []);
-
-        ref.current.insideRender = true;
-        console.log('render start');
-
-        if(!ref.current.subs) {
-            ref.current.subs = render(ref.current.subj.pipe(distinctUntilChanged(compare))).subscribe(next => {
-                ref.current.last = next;
-                if(!ref.current.insideRender) {
-                    ref.current.invalidatedCount += 1;
-                    setInvalidatedCount(ref.current.invalidatedCount);
-                    console.log('setting state');
-                } else {
-                    console.log('skipping state');
-                }
-            });
-        } else {
-            ref.current.subj.next(props);
-        }
-
-        ref.current.insideRender = false;
-        console.log('render end');
-
-        console.log('render', props, _invalidatedCount);
-
-        return ref.current.last;
+    } else {
+        ref.current.subj.next(props);
     }
+
+    ref.current.insideRender = false;
+    console.log('render end');
+
+    console.log('render', props, _invalidatedCount);
+
+    return ref.current.last;
 }
